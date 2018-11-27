362fc7c800465590f5dbbeb3efc814a346ee4bcd
NPGB <giabao050598@gmail.com>
2018-11-27 21:58:43 +0700

    second commit

--- lib/connection.php
+++ lib/connection.php
@@ -1,8 +1,9 @@
 <?php
-	$server_username = "root";
-	$server_password = "warriorIT13:)";
-	$server_host = "localhost";
-	$database = 'yummy_food';
+	$config = include('config/config.php');
+	$server_username = $config['username'];
+	$server_password = $config['password'];
+	$server_host = $config['host'];
+	$database = $config['database'];
 	 
 	$conn = mysqli_connect($server_host,$server_username,$server_password,$database) or die("không thể kết nối tới database");
 	mysqli_query($conn,"SET NAMES 'UTF8'");
